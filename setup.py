b7c659ebbbc76c2525a8ade040338e7a7c65fbc1
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2026-02-16 19:30:24 +0800

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ot_dsim',
-    version='0.1.0',
+    version='0.1.1',
     packages=['ot_dsim', 'ot_dsim.bignum_lib'],
     package_dir={'ot_dsim': '.'},
     install_requires=[
